72d22b12f336c32c3c34a13e7625216d5eb0d38e
cutesd <kimberly@kimberlycooper.me>
2018-06-22 12:34:56 -0700

    added 'see more gifs' button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         var selImg;
         var selNum;
         var topics = [];
+        var newLimit = options.limit;
 
         this.play = init;
 
@@ -67,20 +68,32 @@ $(document).ready(function () {
         /* =================== SEARCH ===================== */
         function newSearch() {
             clearImages();
-            search($(this).text());
+            newLimit = options.limit;
+            search($(this).text(), options.limit);
         }
 
-        function search(str) {
+        function seeMoreGifs(e) {
+            e.preventDefault();
+            console.log("see more gifs");
+            var name = $('.title').text();
+            clearImages();
+            newLimit += options.limit;
+            search(name, newLimit);
+        }
+
+        function search(str, limit) {
             str = verifyStr(str);
+            console.log("search: "+str);
             //
             $.ajax({
-                url: "https://api.giphy.com/v1/gifs/search?q=" + str.toLowerCase() + "&api_key=5aHUfq0wQEZJreua4O5K7J1qBL7S8vzj&limit=" + options.limit,
+                url: "https://api.giphy.com/v1/gifs/search?q=" + str.toLowerCase() + "&api_key=5aHUfq0wQEZJreua4O5K7J1qBL7S8vzj&limit=" + limit,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
                 if (response.data.length > 0) {
                     $('.title').text(titleStr(str));
                     $.each(response.data, buildImg);
+                    addMoreBtn();
                 } else {
                     $('.title').text('Sorry, your search returned 0 results.');
                     $(".d-flex button:last-child").remove()
@@ -91,9 +104,6 @@ $(document).ready(function () {
         /* ===================  IMAGES & INTERACTION ===================== */
         function buildImg(i, val) {
             var still_obj = val.images.fixed_height_still;
-            var __w = still_obj.width;
-            var __h = still_obj.height;
-
 
             var card = $('<div>').addClass('card');
 
@@ -101,8 +111,8 @@ $(document).ready(function () {
             <div class="card-body">
                 <h5 class="card-title">`+ val.title + `</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Rating: `+ val.rating + `</h6>
-                <p class="card-text">Click on the image to see it animate. <br><br>id: `+val.id+` <br>
-                url: <a href="`+val.bitly_url+`" target="_blank">`+val.bitly_url+`</a></p>
+                <p class="card-text">Click on the image to see it animate. <br><br>id: `+ val.id + ` <br>
+                url: <a href="`+ val.bitly_url + `" target="_blank">` + val.bitly_url + `</a></p>
             </div>
             <div class="card-footer">
             <a class="btn btn-danger btn-block" href="`+ val.images.original.url + `" download="giphy.gif">Download</a>
@@ -113,6 +123,13 @@ $(document).ready(function () {
                 var url = $(this).attr('href');
                 window.open(url, '_blank');
             });
+            $('.card-columns').append(card);
+        }
+
+        function addMoreBtn() {
+            var card = $('<div>').addClass('card');
+            card.append(`<div class="card-body"><button class="btn btn-outline-info btn-block">See More Gifs</button></div>`)
+            card.find('button').on("click", seeMoreGifs);
             $('.card-columns').append(card);
 
         }
@@ -140,7 +157,7 @@ $(document).ready(function () {
 
         function clearImages() {
             $('.title').text('');
-            $('.row').empty();
+            $('.card-columns').empty();
         }
 
         /* =================== STRING FORMATTING ===================== */
